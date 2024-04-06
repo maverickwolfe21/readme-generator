f5f4a951dd3c108bfd340a9605d8ae58f1a42639
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-04-05 17:54:35 -0700

    added inquirer prompt with questions

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// inquier setup
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter project title",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter project description",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter installation steps",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Enter usage instructions",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "Choose a license for your project:",
+      name: "license",
+      choices: ["MIT License", "GNU General Public License v3.0", "Apache License 2.0", "None"],
+    },
+    {
+      type: "input",
+      message: "Enter recommended contributions",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Enter project tests",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Enter contact information",
+      name: "contact",
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    // create markdown file and log results
+  ])
+  .then((response) => {
+    console.log("test");
+  });
